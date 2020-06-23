b6c2f6c8cc8afa0f68ed8a33c726e5346a9ab9aa
Kamila Duda <kakus86@o2.pl>
2020-06-23 17:09:39 +0200

    functions in blocks

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-console.log("Witam Cię na mojej stronie :)");
-let changeBackground = document.querySelector(".tile__button");
-let background = document.querySelector(".body");
-let table = document.querySelector(".table");
-let tableTitle = document.querySelector(".table__title");
-changeBackground.addEventListener("click", () => {
-    background.classList.toggle("body__background");
-    if(background.classList.contains("body__background")){
-        table.style.color="black";
-        tableTitle.style.color="white";
-    }else{
-        tableTitle.style.color="black";
+{
+    const hallo = () => {
+        console.log("Witam Cię na mojej stronie :)");
     };
-});
+    const backgroundToggle = () => {
+        const background = document.querySelector(".body");
+        const table = document.querySelector(".table");
+        const tableTitle = document.querySelector(".table__title");
+        background.classList.toggle("body__background");
+        if (background.classList.contains("body__background")) {
+            table.style.color = "black";
+            tableTitle.style.color = "white";
+        } else {
+            tableTitle.style.color = "black";
+        };
+    };
+    const init = () => {
+        const changeBackground = document.querySelector(".tile__button");
+        changeBackground.addEventListener("click", backgroundToggle);
+        hallo();
+    };
+    init();
+}
